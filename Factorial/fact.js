315b9a2dd391cd0a7f28ab934bbedd308670c037
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-07 14:53:46 +0530

    .

--- Factorial/fact.js
+++ Factorial/fact.js
@@ -4,3 +4,16 @@ for(let i = 1; i <= n; i++){
     fact = fact * i;
     console.log(fact);
 }
+
+
+// factorial recursion
+
+function calFact(n){
+    if(n === 0 || n === 1){
+        return 1;
+    } else{
+        return n * calFact(n - 1);
+    }
+}
+
+console.log(calFact(3))
