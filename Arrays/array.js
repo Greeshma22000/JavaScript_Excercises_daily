1d01df091c3d1e39664f2333eaa0897f79d72462
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-23 06:54:58 +0530

    .

--- Arrays/array.js
+++ Arrays/array.js
@@ -1,2 +1,8 @@
 let firstStudent = {name: "Ira", city: "Guntur", country: "India"};
 console.log(firstStudent);
+
+let arrayConstructor = new Array();
+arrayConstructor[0] = 2;
+arrayConstructor[1] = 3;
+arrayConstructor[4] = 3;
+console.log(arrayConstructor);
