4a009ae60758a3b5c86c6533410e64deddcec972
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-23 06:56:07 +0530

    .

--- Arrays/array.js
+++ Arrays/array.js
@@ -6,3 +6,6 @@ arrayConstructor[0] = 2;
 arrayConstructor[1] = 3;
 arrayConstructor[4] = 3;
 console.log(arrayConstructor);
+
+let arrayConstructor2 = new Array("Ram", "Shayam");
+console.log(arrayConstructor2);
